8fea8cb01feda41b6bfcdb017b4f1175a069862a
jichaoqian <jichaoqian@iotogether.com>
2018-06-21 17:58:31 +0800

    Initial Commit

---
+++ ExpandAndDelete/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpandAndDelete
+//
+//  Created by TUOGE on 2018/6/21.
+//  Copyright © 2018年 iotogether. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExpandAndDelete/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExpandAndDelete
+//
+//  Created by TUOGE on 2018/6/21.
+//  Copyright © 2018年 iotogether. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
